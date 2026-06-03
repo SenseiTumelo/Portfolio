b26cac35446ca2af4432526ff4ce424e710ca4de
senseiTumelo <tmotjopi@gmail.com>
2026-06-03 15:29:56 +0200

    added hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+const Hero = () => {
+    return ( 
+    <main className=" w-full overflow -x-hidden">
+
+    </main> );
+}
+ 
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
+import { useGSAP } from "@gsap/react";
+
 const Navbar = () => {
     return (  <nav className="w-full h-16 flex items-center justify-between px-8 fixed top-0 left-0 z-50">
         <div className="text-2xl ">Tumelo.Motjopi</div>
-        <ul className="flex space-x-8">
+        <ul className="flex space-x-25 text-md">
             <li><a href="#home" className="hover:text-gray-500">Home</a></li>
             <li><a href="#about" className="hover:text-gray-500">About</a></li>
             <li><a href="#projects" className="hover:text-gray-500">Projects</a></li>
